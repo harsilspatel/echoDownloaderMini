c99ea59ca1bba7d7502fb9f601a898958ddd56ef
hpat0003 <hpat0003@student.monash.edu>
2018-05-12 02:30:32 +1000

    added the extension

--- download.js
+++ download.js
@@ -2,6 +2,6 @@ var date = document.getElementsByClassName("left")[0].getElementsByClassName("cl
 var name = document.getElementsByClassName("left")[0].getElementsByClassName("classList")[0].innerText.slice(0, 7);
 
 var url = document.querySelectorAll("video")[0]["src"];
-var fullname = name + " " + date;
+var fullname = name + " " + date + ".mp4";
 
 [url, fullname];
